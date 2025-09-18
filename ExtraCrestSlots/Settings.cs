c755a7a7bb0d56dc4265cd6e3e134ef713c43890
Truinto <47917181+Truinto@users.noreply.github.com>
2025-09-18 16:29:58 +0200

    renamed folder

---
+++ ExtraCrestSlots/Settings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Shared.JsonNS;
+using Newtonsoft.Json;
+
+namespace ExtraCrestSlots
+{
+    public class Settings
+    {
+        [JsonIgnore] public string? FilePath;
+
+        [JsonInclude] public int Version = 1;
+
+        public void Save()
+        {
+            JsonTool.SerializeFile(FilePath!, this);
+        }
+
+        public static Settings Load()
+        {
+            string filePath = Path.Combine(BepInEx.Paths.ConfigPath, "ExtraCrestSlots.json");
+            if (JsonTool.DeserializeFile(filePath, out _state))
+            {
+                _state.FilePath = filePath;
+            }
+            else
+            {
+                _state = new();
+                _state.FilePath = filePath;
+                _state.Save();
+            }
+            return _state;
+        }
+
+        private static Settings? _state;
+        public static Settings State => _state ?? Load();
+    }
+}
